0c647c332531f882bf12a944a1d44df2e450b6cf
Omar Rahman <omar@r4men.com>
2026-08-26 02:16:44 -0700

    Custom string validation for colors in config

--- src/main/java/com/r4men/game_knight/util/GKUtil.java
+++ src/main/java/com/r4men/game_knight/util/GKUtil.java
@@ -1,5 +1,20 @@
 package com.r4men.game_knight.util;
 
-public class GKUtil {
+import com.r4men.game_knight.GKConfig;
 
+public class GKUtil {
+    public static int getArgbIntFromRgbaString(String str) {
+        if (str == null || !GKConfig.RGBA_HEX_PATTERN.matcher(str).matches()) {
+            throw new IllegalArgumentException(
+                    "Expected color in #RRGGBBAA format, got: " + str
+            );
+        }
+
+        int red = Integer.parseInt(str.substring(1, 3), 16);
+        int green = Integer.parseInt(str.substring(3, 5), 16);
+        int blue = Integer.parseInt(str.substring(5, 7), 16);
+        int alpha = Integer.parseInt(str.substring(7, 9), 16);
+
+        return (alpha << 24) | (red << 16) | (green << 8) | blue;
+    }
 }
