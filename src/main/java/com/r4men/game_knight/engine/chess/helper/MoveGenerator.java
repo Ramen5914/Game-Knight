72394a6ecb740c76155bc9d504277f9190d8ff76
Omar Rahman <omar@r4men.com>
2026-08-25 17:51:50 -0700

    Added knight move generation

--- src/main/java/com/r4men/game_knight/engine/chess/helper/MoveGenerator.java
+++ src/main/java/com/r4men/game_knight/engine/chess/helper/MoveGenerator.java
@@ -183,6 +183,43 @@ public final class MoveGenerator {
 
     private static int generateKnightMoves(List<Move> moveList, Board board, int from10x12, Piece.Color color) {
         int count = 0;
+        int from8x8 = Util.convert10x12to8x8(from10x12);
+
+        int enPassantSquare10x12 = board.getEnPassantSquare10x12();
+        int castlingRights = board.getCastlingRights();
+        int halfmoveClock = board.getHalfmoveClock();
+
+        final int N = Direction.D10X12.N.toInt();
+        final int E = Direction.D10X12.E.toInt();
+        final int S = Direction.D10X12.S.toInt();
+        final int W = Direction.D10X12.W.toInt();
+        List<Integer> spots = List.of(
+                from10x12 + N + N + W,
+                from10x12 + N + N + E,
+                from10x12 + E + E + N,
+                from10x12 + E + E + S,
+                from10x12 + S + S + E,
+                from10x12 + S + S + W,
+                from10x12 + W + W + S,
+                from10x12 + W + W + N
+        );
+
+        for (int to10x12 : spots) {
+            if (Util.isIntOffBoard10x12(to10x12)) continue;
+
+            final int to8x8 = Util.convert10x12to8x8(to10x12);
+
+            Piece capturedPiece = board.getPieceAt10x12(to10x12);
+            if (capturedPiece.isEmpty()) {
+                if (isLegalMove(moveList, board,
+                        new Move(from8x8, to8x8, Move.Flag.QUIET_MOVE_FLAG, enPassantSquare10x12, castlingRights, halfmoveClock, Piece.EMPTY)
+                )) count++;
+            } else if (!capturedPiece.isOffBoard() && capturedPiece.matchesColor(color.opposite())) {
+                if (isLegalMove(moveList, board,
+                        new Move(from8x8, to8x8, Move.Flag.CAPTURES_FLAG, enPassantSquare10x12, castlingRights, halfmoveClock, capturedPiece)
+                )) count++;
+            }
+        }
 
         return count;
     }
